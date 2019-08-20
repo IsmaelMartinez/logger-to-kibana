499c5c6747abb2caae22cf07fc8e2f6683d3a1f0
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2019-08-20 21:55:22 +0100

    Adding example.py file, more documentation and improving the regex to check single and double quoutes

--- README.md
+++ README.md
@@ -48,3 +48,89 @@ To execute the command run:
 ```bash
 main.py process_and_generate -f <file_location>.py
 ```
+
+## How does it work
+
+Currently this program uses regex `detector` to categorise the different lines.
+
+Then, it usses the `filter` to select what part of the line to keep.
+
+It does this in the following order:
+
+| type | detector | filter |
+|---|---|---|
+| function | `r'def'` | `r'(?<=def ).*?(?=\()'` |
+| critical | `r'LOG.critical'` | `r'(?<=LOG.critical\(["\']).*?(?=["\'])'` |
+| error | `r'LOG.error'` | `r'(?<=LOG.error\(["\']).*?(?=["\'])'` |
+| warn | `r'LOG.warn'` | `r'(?<=LOG.warn\(["\']).*?(?=["\'])'` |
+| info | `r'LOG.info'` | `r'(?<=LOG.info\(["\']).*?(?=["\'])'` |
+| debug | `r'LOG.debug'` | `r'(?<=LOG.debug\(["\']).*?(?=["\'])'` |
+
+For each function, it will generate an object like the following:
+
+```python
+{ '<function_filter>': {
+    'function_name': '<function_filter>',
+    'logs': [{
+        'type': '<type>',
+        'message': '<filter_message>'
+    },{
+        ...
+    }]
+}}
+```
+
+As an example, when processing the following file:
+
+```python
+def lambda_handler(event: dict, context):
+    """
+    Handles a lambda event and does some stuff with it
+    :param context: Unused lambda execution context.
+    :return:
+    """
+    ...
+    LOG.debug('Initialising')
+    ...
+    LOG.info('Processing')
+    ...
+    LOG.warn('Success')
+    ...
+    LOG.error('Failure')
+    ...
+    LOG.critical('Bananas')
+    ...
+)
+```
+
+```python
+{
+    'lambda_handler': {
+        'function_name': 'lambda_handler',
+        'logs': [
+            {
+                'type': 'debug',
+                'message': 'Initialising'
+            },
+            {
+                'type': 'info',
+                'message': 'Processing'
+            },
+            {
+                'type': 'warn',
+                'message': 'Success'
+            },
+            {
+                'type': 'error',
+                'message': 'Failure'
+            },
+            {
+                'type': 'critical',
+                'message': 'Bananas'
+            }
+        ]
+    }
+}
+```
+
+After this, it removes any functions without logs, and generates a kibana metric visualisation with the rest.
--- src/file_processor.py
+++ src/file_processor.py
@@ -2,22 +2,49 @@ import re
 
 results = {}
 
+function_mapping = {
+    "detector": r'def',
+    "filter": r'(?<=def ).*?(?=\()'
+}
+
+log_mapping = [{
+    "type": "debug",
+    "detector": r'LOG.debug',
+    "filter": r'(?<=LOG.debug\(["\']).*?(?=["\'])'
+},{
+    "type": "info",
+    "detector": r'LOG.info',
+    "filter": r'(?<=LOG.info\(["\']).*?(?=["\'])'
+},{
+    "type": "warn",
+    "detector": r'LOG.warn',
+    "filter": r'(?<=LOG.warn\(["\']).*?(?=["\'])'
+},{
+    "type": "error",
+    "detector": r'LOG.error',
+    "filter": r'(?<=LOG.error\(["\']).*?(?=["\'])'
+},{
+    "type": "critical",
+    "detector": r'LOG.critical',
+    "filter": r'(?<=LOG.critical\(["\']).*?(?=["\'])'
+}]
+
 def readFile(filename: str) -> dict:
     with open(filename) as file:
         function_name = None
         for line in file:
-            if re.findall(r'def', line):
-                function_name = re.findall(r'(?<=def ).*?(?=\()',line)[0]
+            if re.findall(function_mapping['detector'], line):
+                function_name = re.findall(function_mapping['filter'],line)[0]
                 results[function_name] = {'function_name': function_name, 'logs': []}
-            elif re.findall(r'LOG.info', line):
-                message = re.findall(r'(?<=LOG.info\(\").*?(?=\")',line)
-                results[function_name]['logs'].append({ 'type': 'info', 'message': message })
-            elif re.findall(r'LOG.error', line):
-                message = re.findall(r'(?<=LOG.error\(\").*?(?=\")',line)
-                results[function_name]['logs'].append({ 'type': 'error', 'message': message })
+            else:
+                process_with_log_mapping(function_name, line)
     return results
 
-def strip_log_type_message(line: str, log_type_message: str) -> str:
-    message = line.strip(' ').strip(log_type_message)
-    message = re.findall(r'.+?(?=\")', message)[0]
-    return message
+def process_with_log_mapping(function_name: str, line: str):
+    for mapping in log_mapping:
+        if re.findall(mapping['detector'], line):
+            message = re.findall(mapping['filter'], line)
+            # import pdb; pdb.set_trace()
+            if message:
+                results[function_name]['logs'].append({ 'type': mapping['type'], 'message': message[0] })
+                return
