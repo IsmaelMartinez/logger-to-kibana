cd1d3e9c307f3d83582023e2a1dc03b5b7b2d669
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2019-08-22 16:21:07 +0100

    Adding initial pypi data

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='logger_to_kibana',
+    version='0.0.1',
+    description='Import logger messages from a file and generates a Kibana Visualisation',
+    packages=setuptools.find_packages(),
+)
