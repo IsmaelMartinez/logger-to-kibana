349324e966413a945b78a49dcafb7a4c6f79e57b
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2019-09-10 22:17:50 +0100

    Adding the parser objects as configurable in the settings.ini file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import src.commands as processor
 
+
 def main():
     """
     Generate Kibana Visualisation From File
     """
     processor.commands()
+
 
 if __name__ == '__main__':
     main()
