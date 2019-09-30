b06649f12ef09783b4a72d016b6cfa7296d8b1c8
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2019-09-30 06:38:51 +0100

    WIP pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='logger_to_kibana',
-    version='0.0.1',
+    version='0.0.2',
     description='Import logger messages from a file and \
                  generates a Kibana Visualisation',
     packages=setuptools.find_packages(),
