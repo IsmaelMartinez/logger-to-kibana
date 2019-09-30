5947073b147d6b44797e7eb62380554bd04eaebd
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2019-09-30 06:58:02 +0100

    upgrading version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='logger_to_kibana',
-    version='0.0.2',
+    version='0.0.3',
     description='Import logger messages from a file and \
                  generates a Kibana Visualisation',
     packages=setuptools.find_packages(),
