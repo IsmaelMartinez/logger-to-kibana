1a33b830bec74aef92dd403406de6beb6821ecbb
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2019-11-07 22:28:11 +0000

    Bump version: 0.6.0 → 0.6.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='logger-to-kibana',
-    version='0.6.0',
+    version='0.6.1',
     description='Import logger messages from a file and \
                  generates a Kibana Visualization',
     author='Ismael Martinez Ramos',
