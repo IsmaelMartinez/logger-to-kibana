0b020c7958cb7783cf45bef3e76423b5473f56f7
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2019-11-28 11:49:20 +0000

    Bump version: 0.7.7 → 0.8.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='logger-to-kibana',
-    version='0.7.7',
+    version='0.8.0',
     description='Import logger messages from a file and \
                  generates a Kibana Visualization',
     author='Ismael Martinez Ramos',
